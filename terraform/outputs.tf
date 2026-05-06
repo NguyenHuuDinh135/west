c547dfb6606c898d21300e8b22f499dcde9d0f93
NguyenHuuDinh135 <dinhknd3@gmail.com>
2026-05-06 11:16:07 +0700

    Update Terraform outputs to reference selected VPC and correct DynamoDB table name; add ECR repository URL output.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = data.aws_vpc.selected.id
 }
 
 output "alb_dns_name" {
@@ -15,9 +15,13 @@ output "ecs_service_name" {
 }
 
 output "dynamodb_table_name" {
-  value = aws_dynamodb_table.user_profiles.name
+  value = aws_dynamodb_table.products.name
 }
 
 output "redis_endpoint" {
   value = aws_elasticache_replication_group.main.primary_endpoint_address
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
 }
